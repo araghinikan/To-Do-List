b70bab8dd83fd920b0a5c6ba10a69bcd42d43b29
araghinikan <araghinikan12@gmail.com>
2025-11-02 17:12:28 +0330

    security base added

---
+++ src/main/java/com/nikan/todolist/model/Role.java
@@ -0,0 +1,6 @@
+package com.nikan.todolist.model;
+
+public enum Role {
+    USER,
+    ADMIN;
+}
---
+++ src/main/java/com/nikan/todolist/service/UserService.java
@@ -0,0 +1,29 @@
+package com.nikan.todolist.service;
+
+import com.nikan.todolist.model.User;
+import com.nikan.todolist.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user  = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("Username not found"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
